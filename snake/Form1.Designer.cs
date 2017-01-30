69b649722291fbc72161234436aaa49a1acc203f
2312olol <2312olol@DESKTOP-LL42G1V>
2017-01-30 14:32:48 +0100

    Add project files.

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(3, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.35F);
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(164, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(406, 404);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ snake/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace snake
+{
+    public partial class Form1 : Form
+    {
+        public snake snake;
+        public food food;
+        public Form1()
+        {
+            InitializeComponent();
+            snake = new snake();
+            food = new food();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            snake.drawMe(e.Graphics);
+            food.drawMe(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!snake.alive) {
+                timer1.Enabled = false;
+                MessageBox.Show("DEAD SNAKE!! Score :" + snake.getScore());
+                snake = new snake();
+                food = new food();
+                timer1.Enabled = true;
+            }
+            else
+            {
+                if (snake.ate)
+                {
+                    snake.ate = false;
+                    food = new food();
+                }
+                snake.move();
+                snake.eat(food.pos);
+                Refresh();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up: snake.endreRetning(0); break;
+                case Keys.Right: snake.endreRetning(1); break;
+                case Keys.Down: snake.endreRetning(2); break;
+                case Keys.Left: snake.endreRetning(3); break;
+                case Keys.P: timer1.Enabled = false; break;
+                case Keys.R: timer1.Enabled = true; break;
+            }
+        }
+        
+    }
+    public class snake
+    {
+        public ArrayList snakePos = new ArrayList();
+        public int retning, retningY, retningX;
+        public Boolean alive = true;
+        public Boolean ate = false;
+        Random r;
+        public snake()
+        {
+            retning = 1;
+            retningY = 0;
+            retningX = 1;
+            snakePos.Add(new Point(6, 0));
+        }
+        public void drawMe(Graphics g)
+        {
+            SolidBrush b = new SolidBrush(Color.Green);
+            for (int i = 0; i < snakePos.Count; i++)
+            {
+                Point point = (Point)snakePos[i];
+                g.FillEllipse(b,point.X*10, point.Y*10, 10, 10);
+            }
+        }
+
+        public void move()
+        {
+            Point nyP = lovligPoint((Point)snakePos[0]);
+            Point p = (Point)snakePos[0];
+            snakePos.Insert(0, nyP);
+            if (snakePos.Count > 1)
+            {
+                snakePos.RemoveAt(snakePos.Count - 1);
+                for (int i = 0; i < snakePos.Count; i++)
+                {
+                    Point pp = (Point)snakePos[i];
+                    if (i > 0 && nyP == pp)
+                    {
+                        alive = false;
+                    }
+                }
+            }
+            
+        }
+
+
+        public Point lovligPoint(Point p)
+        {
+            Point nyP = new Point(p.X + retningX, p.Y + retningY);
+            alive = (nyP.X >= 0 && nyP.X < 50 && nyP.Y >= 0 && nyP.Y < 50);
+            return nyP;
+        }
+
+        public void endreRetning(int r)
+        {
+            if (r % 2 == 0 && retning % 2 != 0 || r % 2 != 0 && retning % 2 == 0)
+            {
+                retning = r;
+                switch (r)
+                {
+                    case 0: retningY = -1; retningX = 0; break;
+                    case 1: retningY = 0; retningX = 1; break;
+                    case 2: retningY = +1; retningX = 0; break;
+                    case 3: retningY = 0; retningX = -1; break;
+                }
+            }
+        }
+        public int getScore()
+        {
+            return snakePos.Count - 1;
+        }
+        public void eat(Point food)
+        {
+            Point p = (Point)snakePos[0];
+            if (food.X == p.X && food.Y == p.Y)
+            {
+                Console.WriteLine("nom nom");
+                snakePos.Add(new Point(food.X, food.Y));
+                ate = true;
+            }
+        }
+    }
+
+    public class food
+    {
+        Random r;
+        public Point pos;
+        public food()
+        {
+            this.r = new Random();
+            pos = new Point(r.Next(0, 40), r.Next(0, 40));
+        }
+        public void drawMe(Graphics g)
+        {
+            SolidBrush b = new SolidBrush(Color.Red);
+            g.FillEllipse(b, pos.X*10, pos.Y*10, 10, 10);
+        }
+    }
+}
